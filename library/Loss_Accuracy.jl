9d94f5455516c2ddfdf0370963baadc57e43335e
Weikang_Li <48015309+LWKJJONAK@users.noreply.github.com>
2022-05-07 14:26:21 +0800

    function update

--- library/Loss_Accuracy.jl
+++ library/Loss_Accuracy.jl
@@ -1,7 +1,6 @@
 # for amplitude encoding
 function acc_loss_rdm_cu(circuit::ChainBlock,reg::ArrayReg,y_batch::Matrix{Float64},batch_size::Int64)
-    reg_ = focus!(copy(reg) |> circuit, (pos_)) |> cpu
-    rdm = density_matrix(reg_).state;
+    rdm = density_matrix(copy(reg) |> circuit, (pos_,)).state
     q_ = zeros(batch_size,2);
     for i=1:batch_size
         q_[i,:] = diag(rdm[:,:,i]) |> real
@@ -16,7 +15,7 @@ end
 
 # for block encoding
 function acc_loss_rdm_cu(nbit::Int64,circuit::Vector,y_batch::Matrix{Float64},batch_size::Int64)
-    rdm = batch([density_matrix(focus!(zero_state(nbit) |> circuit[i], (pos_))).state for i in 1:batch_size])[:,:,1,:]
+    rdm = batch([density_matrix(zero_state(nbit) |> circuit[i], (pos_)).state for i in 1:batch_size])[:,:,1,:]
     q_ = zeros(batch_size,2);
     for i=1:batch_size
         q_[i,:] = diag(rdm[:,:,i]) |> real
